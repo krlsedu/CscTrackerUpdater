8028dba1e991e3f2e52ee1c3f5479fbbd2249354
Carlos Eduardo Duarte Schwalm <carlosschwalm@frigelar.com.br>
2024-01-11 15:14:32 -0300

    Expand handling of dependencies in UpdaterService
    Extended the functionality of the UpdaterService to handle pom.xml files in addition to requirements.txt for updating library versions. Improved error handling and refactored methods for clarity and further abstraction. Modified the remote repository method from update to insert, allowing the insertion of fresh data instead of just updating the existing data.

--- services/updater_service.py
+++ services/updater_service.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import stat
 import subprocess
+import xml.etree.ElementTree as ET
 
 from csctracker_py_core.models.emuns.config import Config
 from csctracker_py_core.repository.http_repository import HttpRepository
@@ -51,13 +52,13 @@ class UpdaterService:
                     if os.path.exists(folder_):
                         shutil.rmtree(folder_, onerror=del_rw)
                     Repo.clone_from(cred_repo_url, folder_)
-                    if self.edit_file(f"{folder_}/requirements.txt", version, lib_name):
+                    if self.edit_dependency_file(folder_, version, lib_name):
                         self.commit_changes(folder_, f"Update {lib_name} to {version}")
                         changes = self.push_changes(folder_)
                         if changes["status"] == "ok":
                             self.logger.info(f"Updated {app_att['app_att']} successfully.")
                             app_att['library_version'] = version
-                            self.remote_repository.update("libraries", ['id'], app_att, headers)
+                            self.remote_repository.insert("libraries", data=app_att, headers=headers)
                         if os.path.exists(folder_):
                             os.chmod(folder_, stat.S_IWRITE)
                             try:
@@ -101,7 +102,17 @@ class UpdaterService:
             returns_.append({"status": "error"})
         return returns_
 
-    def edit_file(self, file_path, version, lib_name):
+    def edit_dependency_file(self, file_path_, version, lib_name):
+        file_path = f"{file_path_}/requirements.txt"
+        if not os.path.exists(file_path):
+            file_path = f"{file_path_}/pom.xml"
+            if not os.path.exists(file_path):
+                self.logger.error(f"Dependencies file not found")
+                raise Exception(f"Dependencies file not found")
+            return self.edit_pom_xml(file_path, version, lib_name)
+        return self.edit_requirements_file(file_path, version, lib_name)
+
+    def edit_requirements_file(self, file_path, version, lib_name):
         try:
             with open(file_path, 'r') as file:
                 lines = file.readlines()
@@ -130,6 +141,43 @@ class UpdaterService:
             msg_ = f"An error occurred while editing the file: {str(e)}"
             self.logger.error(msg_)
             raise Exception(msg_)
+
+    def edit_pom_xml(self, file_path, version, lib_name):
+        try:
+            tree = ET.parse(file_path)
+            root = tree.getroot()
+
+            initial_tree_as_string = ET.tostring(root, encoding='utf8').decode('utf8')
+
+            namespaces = {'': 'http://maven.apache.org/POM/4.0.0'}
+
+            ET.register_namespace('', 'http://maven.apache.org/POM/4.0.0')  # Register the namespace
+
+            if lib_name.endswith("-parent") or lib_name.endswith("-starter"):
+                self.logger.info("Parent project found.")
+                for parent in root.findall('parent/version', namespaces):
+                    parent.text = version
+
+            elif lib_name.endswith("-dependency"):
+                self.logger.info("Dependency project found.")
+                for dependency in root.findall('dependencies/dependency', namespaces):
+                    artifact_id = dependency.find('artifactId', namespaces).text
+                    if artifact_id == lib_name:
+                        version_element = dependency.find('version', namespaces)
+                        version_element.text = version
+
+            updated_tree_as_string = ET.tostring(root, encoding='utf8').decode('utf8')
+
+            if initial_tree_as_string == updated_tree_as_string:
+                self.logger.info("No changes were made to the file.")
+                return False
+
+            tree.write(file_path)
+            self.logger.info("File edited successfully.")
+            return True
+        except Exception as e:
+            self.logger.error(f"An error occurred while editing the XML file: {str(e)}")
+            raise Exception(f"An error occurred while editing the XML file: {str(e)}")
 
     def commit_changes(self, repo_dir, commit_message):
         try:
